2110ef5dd3d9e661e95ccdd8c41bb11dc200ab5d
Cheryl Casas <Cherylccasas@gmail.com>
2023-06-19 01:37:31 -0600

    Updated Enter Page

--- client/src/components/EnterSite.jsx
+++ client/src/components/EnterSite.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import Enter from "../images/Enter.jpg"
+import Styled from "styled-components"
 
 const EnterSite = () => {
   return (
     <div>
-      <header className="App-header">"PIGMENT POND"
-       <Link to="/Home" style={{ textDecoration: 'none' , color: 'red', fontFamily: 'Roboto' }}>ENTER</Link>
-      </header>
+      <Text>
+      <h1>PIGMENT POND</h1>
+       <Link to="/Home" className="link" style={{ fontSize: '500px' }}>ENTER</Link>
+      </Text>
+      <img src={Enter} alt="Enter" width="100%"/>
     </div>
   )
 }
 
 export default EnterSite
+
+const Text = Styled.div
+`
+position: absolute;
+z-index: 2;
+font-size: 90px;
+width: 100%;
+margin-top: 25px;
+color: yellow;
+font-weight: 900;
+`
