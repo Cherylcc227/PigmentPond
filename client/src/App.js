701f143e869ead999c5042a9a45f816d8bd122e2
Cheryl Casas <Cherylccasas@gmail.com>
2023-06-19 01:00:43 -0600

    Restored Images

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import LandingPage from "./components/LandingPage.jsx";
+import AboutPigmentPond from "./components/AboutPigmentPond.jsx";
+import ContactPage from "./components/ContactPage.jsx";
+import Gallery from "./components/Gallery.jsx";
+import Services from "./components/Services.jsx";
+import ShowDeck from "./components/ShowDeck.jsx";
+import EnterSite from "./components/EnterSite.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route path="/" element={<EnterSite />} />
+            <Route path="/Home" element={<LandingPage />} />
+            <Route path="/AboutPigmentPond" element={<AboutPigmentPond />} />
+            <Route path="/Contact" element={<ContactPage />} />
+            <Route path="/Gallery" element={<Gallery />} />
+            <Route path="/Services" element={<Services />} />
+            <Route path="/ShowDeck" element={<ShowDeck />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
