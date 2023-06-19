ab6ef7c4be2c20b386d1778b09ad305ec7811bae
Cheryl Casas <Cherylccasas@gmail.com>
2023-06-19 15:13:21 -0600

    Modified footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div style={{  background: "black", height: "94px", textAlign: "end", paddingTop: "36px", paddingRight:"30px"}}>
       ™ Pigment Pond 2023
-      {/* <br/> Website created by Area 52 Designs <SocialIcon url="https://www.instagram.com/morseloflove/" /> */}
+      <br/> Website created by Area 52 Designs <SocialIcon url="https://www.instagram.com/morseloflove/" />
     </div>
   )
 }
